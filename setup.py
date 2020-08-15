14ea2d97d557465797d453d40fb340db3fd84fd1
buzz <buzz@users.noreply.github.com>
2020-08-15 13:18:45 +0200

    Update setup.py
    - Require >=Python 3.2.
    - Add dependencies.
    - Add package data.
    - Add classifiers.
    - Update entry point.

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@
 """setup.py for devilspy"""
 
 import re
+import sys
 from setuptools import find_packages, setup
 
+if sys.version_info < (3, 2):
+    raise SystemExit("devilspy requires Python 3.2 or later.")
 
 # parse version (setup.py should not import module!)
 def get_version():
@@ -24,7 +27,21 @@ setup(
     author="buzz",
     author_email="buzz@users.noreply.github.com",
     license="GPLv2",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Environment :: No Input/Output (Daemon)",
+        "Environment :: X11 Applications",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Desktop Environment",
+    ],
     url="https://buzz.github.io/devilspy/",
     packages=find_packages(),
-    entry_points={"gui_scripts": ["devilspy = devilspy.__main__:main"]},
+    install_requires=["click", "PyGObject", "PyYAML"],
+    include_package_data=True,
+    entry_points={"console_scripts": ["devilspy = devilspy.__main__:main"]},
 )
