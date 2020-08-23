a744e064af2bcf4746b38be00e1aad009d1f3b66
buzz <buzz@users.noreply.github.com>
2020-08-23 04:06:28 +0200

    v0.1.0

--- devilspy/meta.py
+++ devilspy/meta.py
@@ -1,7 +1,7 @@
 """Version string used by program and setup.py (using regex parsing)."""
 from gi.repository import Gtk
 
-VERSION = "0.0.4"
+VERSION = "0.1.0"
 PROGRAM_NAME = "devilspy"
 COPYRIGHT = "(c) buzz"
 LICENSE = Gtk.License.GPL_2_0
